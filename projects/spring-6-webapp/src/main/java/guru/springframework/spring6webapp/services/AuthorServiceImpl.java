ecb0cd91327cb71e78332173c7f11082909daefd
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-15 12:36:39 +0700

    add authors view

--- projects/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
+++ projects/spring-6-webapp/src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
@@ -3,18 +3,20 @@ package guru.springframework.spring6webapp.services;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.spring6webapp.domains.Author;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
-    private final AuthorService authorService;
+    private final AuthorRepository authorRepository;
 
     public AuthorServiceImpl(AuthorService authorService) {
-        this.authorService = authorService;
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     @Override
     public Iterable<Author> findAll() {
-        return this.authorService.findAll();
+        return this.authorRepository.findAll();
     }
 
 }
