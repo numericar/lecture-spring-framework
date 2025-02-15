f8b95014219d8e89561e81b3294d8dd054f81f4f
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-15 16:34:33 +0700

    Update LecturesApplicationTests.java

--- projects/dependency-lectures/src/test/java/guru/dependen/lectures/LecturesApplicationTests.java
+++ projects/dependency-lectures/src/test/java/guru/dependen/lectures/LecturesApplicationTests.java
@@ -1,10 +1,24 @@
 package guru.dependen.lectures;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import guru.dependen.lectures.controllers.MyController;
 
 @SpringBootTest
 class LecturesApplicationTests {
+
+	@Autowired // เป็นการ inject คลาส ApplicationContext ที่ถูกสร้างขึ้นมาให้เรา โดย Spring Boot ซึ่ง object จะถูกเก็บอยู่ใน bean
+	ApplicationContext applicationContext;
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
